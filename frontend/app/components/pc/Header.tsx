50fc043deaa487e604eefab66c5be1aec9e1fdae
yuanze <qicnan@gmail.com>
2025-08-23 13:56:24 +0800

    feat: 完成导航栏搜索跳转

--- frontend/app/components/pc/Header.tsx
+++ frontend/app/components/pc/Header.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material';
+import { IconButton, Stack, TextField } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -55,47 +55,52 @@ const Header: React.FC = async () => {
         <NavigationMenu menuData={menuData} />
 
         {/* 搜索框 */}
-        <Box
-          sx={{
-            width: '240px',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            mr: { xs: 0, sm: 1, md: 2, lg: 3, xl: 4 },
-          }}
-        >
-          <TextField
-            variant='outlined'
-            size='small'
-            placeholder=''
+        <form action='/search' method='GET'>
+          <Stack
+            direction='row'
+            spacing={2}
             sx={{
-              width: { xs: 120, sm: 150, md: 180 },
-              height: { xs: 28, md: 30 },
-              '& .MuiOutlinedInput-root': {
-                height: { xs: 28, sm: 30, md: 32 },
-                fontSize: { xs: 14, sm: 15, md: 16 },
-                color: STANDARD_TEXT_COLOR,
-                borderRadius: 1,
-                px: { xs: 1, sm: 1.5, md: 2 },
-                backgroundColor: 'transparent',
-                '& fieldset': {
-                  borderColor: '#cfd8dc',
-                },
-                '&:hover fieldset': {
-                  borderColor: MAIN_BLUE_COLOR,
-                },
-                '&.Mui-focused fieldset': {
-                  borderColor: MAIN_BLUE_COLOR,
-                },
-              },
-              '& input': {
-                p: 0,
-                height: '100%',
-              },
+              width: '240px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              mr: { xs: 0, sm: 1, md: 2, lg: 3, xl: 4 },
             }}
-          />
-          <SearchIcon />
-        </Box>
+          >
+            <TextField
+              variant='outlined'
+              size='small'
+              placeholder=''
+              name='query'
+              sx={{
+                width: { xs: 120, sm: 150, md: 180 },
+                height: { xs: 28, md: 30 },
+                '& .MuiOutlinedInput-root': {
+                  height: { xs: 28, sm: 30, md: 32 },
+                  fontSize: { xs: 14, sm: 15, md: 16 },
+                  color: STANDARD_TEXT_COLOR,
+                  borderRadius: 1,
+                  px: { xs: 1, sm: 1.5, md: 2 },
+                  backgroundColor: 'transparent',
+                  '& fieldset': {
+                    borderColor: '#cfd8dc',
+                  },
+                  '&:hover fieldset': {
+                    borderColor: MAIN_BLUE_COLOR,
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: MAIN_BLUE_COLOR,
+                  },
+                },
+                '& input': {
+                  p: 0,
+                  height: '100%',
+                },
+              }}
+            />
+            <IconButton type='submit' children={<SearchIcon />} />
+          </Stack>
+        </form>
       </Toolbar>
     </AppBar>
   );
